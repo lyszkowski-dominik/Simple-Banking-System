284b74592b77edf9b5096f3fa18fc2245af2c142
lyszkowski-dominik <57920532+lyszkowski-dominik@users.noreply.github.com>
2021-08-23 15:42:44 +0200

    Banking System stage 2 Completed
    implemented Luhn algorithm to output valid credit card numbers

--- Simple Banking System/task/banking/banking.py
+++ Simple Banking System/task/banking/banking.py
@@ -11,7 +11,7 @@ def main_menu():
         print('1. Create an account')
         print('2. Log into account')
         print('0. Exit')
-        
+
         selection = input()
 # Option number 1 - creating credit card and generating pin
         if selection == '1':
@@ -66,9 +66,9 @@ def account_menu(credit_card):
 
 
 class CreditCard:
-    
+
     def __init__(self):
-        self.card_number = '400000' + str(randint(100000000, 999999999)) + str(randint(0, 9))
+        self.card_number = card_number_creator()
         self.pin_number = randint(1000, 9999)
         self.balance = 0
 
@@ -80,6 +80,32 @@ class CreditCard:
 
     def get_balance(self):
         return self.balance
+
+
+def card_number_creator():
+    temporary_card_number = '400000' + str(randint(100000000, 999999999))
+    card_number_list = []
+    counter = 0
+    for x in temporary_card_number:
+        card_number_list.append(x)
+    i = 0
+    while i < len(card_number_list):
+        if (i % 2) == 0:
+            card_number_list[i] = int(card_number_list[i]) * 2
+            if card_number_list[i] > 9:
+                card_number_list[i] -= 9
+        card_number_list[i] = int(card_number_list[i])
+        i += 1
+    sum_of_digits = sum(card_number_list)
+    if sum_of_digits % 10 == 0:
+        card_number_list.append(0)
+    else:
+        modulo = sum_of_digits % 10
+        while modulo % 10 != 0:
+            modulo += 1
+            counter += 1
+    card_number = temporary_card_number + str(counter)
+    return card_number
 
 
 # Starting the actual program
