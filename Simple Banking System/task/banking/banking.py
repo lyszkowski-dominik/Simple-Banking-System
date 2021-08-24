cd560ce43c465117118f27445b27c60384c29257
lyszkowski-dominik <57920532+lyszkowski-dominik@users.noreply.github.com>
2021-08-24 21:43:06 +0200

    Stage 4
    fixed minor bugs.
    to implement : Luhn algorithm to validate given card number

--- Simple Banking System/task/banking/banking.py
+++ Simple Banking System/task/banking/banking.py
@@ -2,15 +2,8 @@ from random import randint
 import sqlite3
 conn = sqlite3.connect('card.s3db')
 cur = conn.cursor()
-# cur.execute('CREATE TABLE card (id INTEGER , number TEXT, pin TEXT, balance INTEGER DEFAULT 0 );')
-# cur.execute('DROP TABLE card')
-# conn.commit()
-cur.execute('SELECT id AS id, number AS number, pin AS pin, balance AS balance FROM card')
-print(cur.fetchall())
 
-credit_card_list = []
-
-
+#cur.execute('DROP TABLE card')
 # Function responsible for displaying main menu
 def main_menu():
     while True:
@@ -47,6 +40,8 @@ def main_menu():
                     break
                 else:
                     print('Wrong card number or PIN!')
+            else:
+                print('Wrong card number or PIN!')
 # Option number 0 - exiting the main menu loop
         elif selection == '0':
             print('Bye!')
@@ -60,6 +55,9 @@ def main_menu():
 def account_menu(account_number):
     while True:
         print('1. Balance')
+        print('2. Add income')
+        print('3. Do transfer')
+        print('4. Close account')
         print('5. Log out')
         print('0. Exit')
         selection = input()
@@ -79,6 +77,15 @@ def account_menu(account_number):
             cur.execute("UPDATE card SET balance = " + balance + " WHERE number = " + account_number + "")
             conn.commit()
             print('Income was added!')
+        elif selection == '3':
+            card_number = input('Enter card number:')
+            do_transfer(card_number, account_number)
+        elif selection == '4':
+            cur.execute("DELETE from card WHERE number = " + account_number + "")
+            conn.commit()
+            print('The account has been closed!')
+            main_menu()
+            break
         elif selection == '5':
             print('You have successfully logged out!')
             main_menu()
@@ -133,5 +140,58 @@ def clean_sql_query(word):
     return word
 
 
+def check_if_table_exists():
+    cur.execute("SELECT count(name) FROM sqlite_master WHERE type='table' AND name='card'")
+    result = str(cur.fetchone())
+    result = clean_sql_query(result)
+    result = int(result)
+    if result == 1:
+        print('Successfully connected to database')
+    else:
+        print('Database does not exist. Creating one')
+        cur.execute('CREATE TABLE card (id INTEGER , number TEXT, pin TEXT, balance INTEGER DEFAULT 0 );')
+        conn.commit()
+
+
+def print_whole_table():
+    cur.execute('SELECT  number AS number, pin AS pin, balance AS balance FROM card')
+    print(cur.fetchall())
+
+
+# have to implement Luhn algorithm to validate given card number
+def do_transfer(card_number, account_number):
+    cur.execute("SELECT number FROM card WHERE number = " + card_number + "")
+    acc_number = str(cur.fetchone())
+    acc_number = clean_sql_query(acc_number)
+    if account_number == card_number:
+        print("You can't transfer money to the same account!")
+    elif int(acc_number) == int(card_number):
+            to_transfer = int(input('Enter how much money you want to transfer:'))
+            cur.execute("SELECT balance FROM card WHERE number = " + account_number + "")
+            my_balance = str(cur.fetchone())
+            my_balance = clean_sql_query(my_balance)
+            my_balance = int(my_balance)
+            if my_balance > to_transfer:
+                my_balance -= to_transfer
+                my_balance = str(my_balance)
+                cur.execute("UPDATE card SET balance = " + my_balance + " WHERE number = " + account_number + "")
+                conn.commit()
+                cur.execute("SELECT balance FROM card WHERE number = " + card_number + "")
+                other_balance = str(cur.fetchone())
+                other_balance = clean_sql_query(other_balance)
+                other_balance = int(other_balance)
+                other_balance += to_transfer
+                other_balance = str(other_balance)
+                cur.execute("UPDATE card SET balance = " + other_balance + " WHERE number = " + card_number + "")
+                conn.commit()
+                print('Success!')
+            else:
+                print('Not enough money!')
+    else:
+        print('Such a card does not exist')
+
+
 # Starting the actual program
+check_if_table_exists()
+print_whole_table()
 main_menu()
